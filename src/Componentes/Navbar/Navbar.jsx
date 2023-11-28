6a00605ac9914da4cc3927e67169c2abf87d8b71
Jonathan Gonzalez <queleimporta545@gmail,com>
2023-11-28 11:11:02 -0500

    subiendo proyecto

---
+++ src/Componentes/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import Navbar_superior from "./Navbar_superior"
+
+const Navbar = () => {
+  return (
+    <header>
+        <Navbar_superior/>
+
+        <div>
+        <h1>prue</h1>
+        </div>
+
+    </header>
+  )
+}
+
+export default Navbar
---
+++ src/Componentes/Navbar/Navbar_superior.jsx
@@ -0,0 +1,21 @@
+import './Navbar_superior.css';
+
+
+function Navbar_superior() {
+  return (
+    <header>
+      <div>
+        <ul>
+          <li>my cuenta</li>
+          <li>favoritos</li>
+          <li>oferta</li>
+          <li>oferta2</li>
+          <li>oferta3</li>
+          <li>carrito</li>
+        </ul>
+      </div>
+    </header>
+  );
+}
+
+export default Navbar_superior
