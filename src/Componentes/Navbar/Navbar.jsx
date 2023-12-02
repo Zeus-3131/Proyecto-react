f543104493fe98028ff2d806975d513dc4eed95b
Jonathan Gonzalez <queleimporta545@gmail,com>
2023-12-02 16:19:34 -0500

    actualizacion de el navbar

--- src/Componentes/Navbar/Navbar.jsx
+++ src/Componentes/Navbar/Navbar.jsx
@@ -1,16 +1,27 @@
 import Navbar_Superior from "./Navbar_superior";
 import Navbar_Intermedio from "./Navbar_Intermedio";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <header>
         <Navbar_Superior/>
-        <div>
-
-        </div>
-
         <Navbar_Intermedio/>
 
+        <div className="contenedorNavbar">
+          <div className="contenidoNav">
+            <nav className="nav">
+              <ul className="ulnav">
+                <li className="linav"><a className="anav" href="#">Inicio</a></li>
+                <li className="linav"><a className="anav" href="#">Tienda</a></li>
+                <li className="linav"><a className="anav" href="#">Hombre</a></li>
+                <li className="linav"><a className="anav" href="#">Top Productos</a></li>
+                <li className="linav"><a className="anav" href="#">Mujer</a></li>
+                <li className="linav"><a className="anav" href="#">Blogs</a></li>
+              </ul>
+            </nav>
+          </div>
+        </div>
     </header>
   )
 }
