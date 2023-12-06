65f1e7b3431445fc331667a8167afbeb4f306991
Jonathan Gonzalez <queleimporta545@gmail,com>
2023-12-06 17:00:20 -0500

    creacion de la segunda seccion y avances

--- src/Componentes/Cuerpo/Inicio/Inicio.jsx
+++ src/Componentes/Cuerpo/Inicio/Inicio.jsx
@@ -1,24 +1,44 @@
 import "./Inicio.css";
 
 const Inicio = () => {
-  return (
+    return (
 
-    <section>
-        <div className="contenedorSection1"> 
-            <div className="contenidoSeccion1">
-                <h1>La mejor<br/>Coleccion del <b style={{color:"red"}}>2024</b></h1>
-                <br />
-                <p className="pSecccion1"><h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis, libero veritatis laudantium vitae sequi aperiam dolorum eum ut rem harum cupiditate doloremque. Nihil saepe ratione et voluptates aut maiores alias.10</h3></p>
-                <br />
-                <button className="botonSeccion1">Compra Ahora</button>
-            </div>
-            <div className="contenidoSeccion2">
-                <img src="./logo2.png" alt="prueba" />
-            </div>
-        </div>
-    </section>
+        <>
+            <section className="contenedorSection1">
+                <div className="contenidoSeccion1">
+                    <h1>La mejor<br />Coleccion del <b style={{ color: "red" }}>2024</b></h1>
+                    <br />
+                    <p className="pSecccion1"><h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis, libero veritatis laudantium vitae sequi aperiam dolorum eum ut rem harum cupiditate doloremque. Nihil saepe ratione et voluptates aut maiores alias.10</h3></p>
+                    <br />
+                    <button className="botonSeccion1">Compra Ahora</button>
+                </div>
+                <div className="contenidoSeccion-1">
+                    <img src="./logo2.png" alt="prueba" />
+                </div>
+            </section>
 
-  )
+            <section className="contenidoSeccion2">
+                <div className="conteSeccion2">
+                        <div className="contenedorpromo">
+                            <div className="promocion1">
+                                <img className="imgpromo1" src="https://tabbase.myshopify.com/cdn/shop/files/1_5.jpg?v=1650439204" alt="zapatillas negras " />
+                                <div className="textpromo">
+                                    <div>
+                                        <h2 className="promoh">Las mejores colecciones</h2>
+                                        <h4 className="promoh">Colección de zapatos de cuero</h4>
+                                        <h5 className="promoh5"><strong>50% de descuento</strong></h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    <div>
+
+                    </div>
+                </div>
+            </section>
+        </>
+
+    )
 }
 
 export default Inicio
--- src/Componentes/Navbar/Navbar_Intermedio.jsx
+++ src/Componentes/Navbar/Navbar_Intermedio.jsx
@@ -3,17 +3,17 @@ import './Navbar_Intermedio.css'
 
 const Navbar_Intermedio = () => {
   return (
-    <div className='contenedorNavbarIntermedio'> 
+    <div className='contenedorNavbarIntermedio'>
       <div className='contenidoIntermedio'>
         <div className='contenidoInter'><p>SALE: 50% OFF THE SUMMER COLLECTION &nbsp; <a href="/collections" title="All Collections"><strong>Shop&nbsp;now&nbsp;&gt;</strong></a></p></div>
-        <div className='contenidoInter'><img className='icono' src="../../../../public/logo.png" alt="icono de la marca" /></div>
+        <div className='contenidoInter'><img className='icono' src="./logo.png" alt="icono de la marca" /></div>
         <div className='contenidoInter'>
-        <form className='contenedorBuscador' >
-					<input className='buscador' type="search"  placeholder="Buscar"/>
-					<button type="submit" className='botonBuscar'>
-						<i class="fa fa-search"></i>
-					</button>
-				</form>
+          <form className='contenedorBuscador' >
+            <input className='buscador' type="search" placeholder="Buscar" />
+            <button type="submit" className='botonBuscar'>
+              <i class="fa fa-search"></i>
+            </button>
+          </form>
         </div>
       </div>
     </div>
