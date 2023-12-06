05db047290dbe819cee244a81a85048e259e5de4
Jonathan Gonzalez <queleimporta545@gmail,com>
2023-12-05 22:13:06 -0500

    creando la primera seccion

--- src/Componentes/Cuerpo/Inicio/Inicio.jsx
+++ src/Componentes/Cuerpo/Inicio/Inicio.jsx
@@ -6,13 +6,13 @@ const Inicio = () => {
     <section>
         <div className="contenedorSection1"> 
             <div className="contenidoSeccion1">
-                <h1>La mejor<br/>Coleccion del 2024</h1>
+                <h1>La mejor<br/>Coleccion del <b style={{color:"red"}}>2024</b></h1>
                 <br />
-                <p style={{width:"50%"}}><h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis, libero veritatis laudantium vitae sequi aperiam dolorum eum ut rem harum cupiditate doloremque. Nihil saepe ratione et voluptates aut maiores alias.10</h3></p>
+                <p className="pSecccion1"><h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis, libero veritatis laudantium vitae sequi aperiam dolorum eum ut rem harum cupiditate doloremque. Nihil saepe ratione et voluptates aut maiores alias.10</h3></p>
                 <br />
-                <button>Compra Ahora</button>
+                <button className="botonSeccion1">Compra Ahora</button>
             </div>
-            <div className="contenidoSeccion-1">
+            <div className="contenidoSeccion2">
                 <img src="./logo2.png" alt="prueba" />
             </div>
         </div>
