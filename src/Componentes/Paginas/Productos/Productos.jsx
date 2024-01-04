c6093a950af1c3bcf71f1e2900f682c21e1c782d
Jonathan Gonzalez <queleimporta545@gmail,com>
2024-01-03 22:53:54 -0500

    poniendo el header provisional de la segunda entrega

--- src/Componentes/Paginas/Productos/Productos.jsx
+++ src/Componentes/Paginas/Productos/Productos.jsx
@@ -1,9 +1,14 @@
-import Navbar from "../../Navbar/Navbar"
-import ItemListContainer from "../../ItemListContainer/ItemListContainer"
-import { NavLink } from "react-router-dom"
+import Navbar from "../../Navbar/Navbar";
+import ItemListContainer from "../../ItemListContainer/ItemListContainer";
+import { NavLink } from "react-router-dom";
 import "./Productos.css";
 import Aside from "../../Aditamentos/Aside/Aside";
 import Footer from "../../Footer/Footer";
+
+// segunda entrega
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ItemDetailContainer from "../../ItemDetailContainer/ItemDetailContainer";
+
 
 const Productos = () => {
   return (
@@ -12,8 +17,12 @@ const Productos = () => {
       <div className="ContenedorNavegacionProductos">
         <nav className="navProductos">
           <div className="col-12">
-            <NavLink className="enlaceP" to="/">Inicio</NavLink>
-            <span><i class="fa fa-angle-right" /></span>
+            <NavLink className="enlaceP" to="/">
+              Inicio
+            </NavLink>
+            <span>
+              <i class="fa fa-angle-right" />
+            </span>
             <span>Calzado especial</span>
           </div>
         </nav>
@@ -24,16 +33,44 @@ const Productos = () => {
         <div className="contenedormainProductos">
           <Aside />
           <div className="contenedorDeProductos">
+            <div>
+              <h3>header provisional</h3>
+              <header>
+                <nav>
+                  <ul>
+                    <NavLink to="/productos">inicio</NavLink><br />
+                    <NavLink to="/categoria/:idCategoria">puma</NavLink><br />
+                    <NavLink to="/categoria/:idItem">nike</NavLink>
+                  </ul>
+                </nav>
+              </header>
+            </div>
+            {/* <header>
+              <h3>header provisional</h3>
+              <BrowserRouter>
+                <Routes>
+                  <Route path="/productos" element={<ItemListContainer />} />
+                  <Route
+                    path="/categoria/:idCategoria"
+                    element={<ItemListContainer />}
+                  />
+                  <Route
+                    path="/categoria/:idItem"
+                    element={<ItemDetailContainer />}
+                  />
+                </Routes>
+              </BrowserRouter>
+            </header> */}
+            <br /><br /><br />
             <ItemListContainer />
           </div>
         </div>
       </main>
 
-      <Footer/>
+      <Footer />
       {/* <h1>productos online</h1> */}
-
     </>
-  )
-}
+  );
+};
 
-export default Productos
+export default Productos;
